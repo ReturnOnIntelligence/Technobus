7cf79b2180f068e0a2ac3688cceb7e8194eab01a
Arck1 <crosts@mail.ru>
2018-02-21 23:34:41 +0300

    remove watemark from img

--- develop/app/js/components/links.js
+++ develop/app/js/components/links.js
@@ -1,7 +1,7 @@
 $(function () {
    $('.js-mobile-number').click(function () {
       if(device.mobile() && (device.android() || device.ios())){
-         window.open('tel: +78123180055');
+         //window.open('tel: +78123180055');
       }
    });
 });
